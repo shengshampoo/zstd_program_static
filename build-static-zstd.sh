97553fd0252524d5af49187a0eb70708c413ea7a
shengshampoo <shengzh@tutanota.com>
2026-01-31 13:16:36 +0800

    Update build-static-zstd.sh

--- build-static-zstd.sh
+++ build-static-zstd.sh
@@ -11,13 +11,15 @@ mkdir -p /work/artifact
 cd $WORKSPACE
 aa=$(curl -s "https://api.github.com/repos/tukaani-project/xz/releases/latest" | grep -Po '"tag_name": "v\K[0-9rc.]+')
 curl -sL $( curl -s "https://api.github.com/repos/tukaani-project/xz/releases/latest" | jq -r '.assets[] | select(.content_type == "application/x-xz") | {browser_download_url}  | .browser_download_url ') | tar x --xz
-cd xz-$aa && CFLAGS="$CFLAGS -static" LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr/local/xzmm --disable-shared --enable-static --enable-year2038
+cd xz-$aa
+CFLAGS="$CFLAGS -static" LDFLAGS="-static --static -no-pie -s" ./configure --prefix=/usr/local/xzmm --disable-shared --enable-static --enable-year2038
 make
 make install
 
 # lz4
 cd $WORKSPACE
-git clone https://github.com/lz4/lz4.git && cd lz4/build/meson
+git clone https://github.com/lz4/lz4.git
+cd lz4/build/meson
 PKG_CONFIG_PATH=/usr/local/xzmm/lib/pkgconfig CFLAGS="$CFLAGS -static" LDFLAGS="-static --static -no-pie -s" meson setup builddir -Dprefix=/usr/local/xzmm -Ddefault_library=static -Dprograms=true --strip
 cd builddir
 sed -i 's@.so.3 @.a @g' ./build.ninja
